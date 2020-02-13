69e7d17b7623a106ae9b170d16e4c663da7cbdda
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-13 11:15:17 +0700

    Fix rendering headers in responses

--- source/http/response.lua
+++ source/http/response.lua
@@ -9,7 +9,7 @@ end
 
 function Response:__tostring()
   local raw = "HTTP/1.1 " .. tostring(self.status) .. "\r\n"
-  for key, value in ipairs(self.headers) do
+  for key, value in pairs(self.headers) do
     raw = raw .. tostring(key) .. ": " .. tostring(value) .. "\r\n"
   end
   return raw .. "\r\n" .. tostring(self.body)
