2d649a00bde05f484835a987007c63c6624f1d7b
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-24 10:33:10 +0000

    Add better error handling

--- source/http/request.lua
+++ source/http/request.lua
@@ -13,6 +13,8 @@ end
 
 function readStatus(client)
   local status_line, err = client:receive()
+  if err then return nil, err end
+
   -- FIXME: won't work with parameters!
   local method, path, params, http_version = status_line:
     match("^([A-Z]+)%s([/%%0-9a-z.-]*)%??([a-z0-9&=]*)%sHTTP/([0-9.]*)$")
@@ -31,7 +33,8 @@ function readHeaders(client)
     if key then
       headers[key] = value
     end
-  until line == '' or err
+  until line == ''
+
   return headers, err
 end
 
@@ -42,11 +45,16 @@ end
 
 function Request.fromSocket(client)
   local method, path, params, version, err = readStatus(client)
+  if err then return nil, err end
+
   local headers, err = readHeaders(client)
+  if err then return nil, err end
+
   local body = ''
   if headers['Content-Length'] then
     body, err = readBody(client, tonumber(headers['Content-Length']))
   end
+  if err then return nil, err end
 
   return Request:new({
     method=method,
