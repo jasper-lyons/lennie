285b441e436c61fb19b774c877ae1c716dc302e5
Jasper Lyons <jasper.lyons@gmail.com>
2020-03-18 17:40:14 +0000

    Use percent encoding in form data

--- source/http/formdata.lua
+++ source/http/formdata.lua
@@ -1,23 +1,25 @@
 require('relative_require')
 
 require('../extension/string')
+local PercentEncoding = require('./percent_encoding')
 
 local formdata = {
   tostring = function (object)
     local container = {}
 
     for key, value in pairs(object) do
-      table.insert(container, key .. '=' .. value)
+      table.insert(container, PercentEncoding.encode(key) .. '=' .. PercentEncoding.encode(value))
     end
 
     return table.concat(container, '&')
   end,
+
   parse = function (string)
     local container = {}
 
     for _, pairString in pairs(string:split('&')) do
       local key, value = table.unpack(pairString:split('='))
-      container[key] = value
+      container[PercentEncoding.decode(key)] = PercentEncoding.decode(value)
     end
 
     return container
